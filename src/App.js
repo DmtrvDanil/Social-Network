9842134af34bb13a8085f43b22d981e425e8bbe3
Danil <dmtrvd90@gmail.com>
2023-01-22 17:43:36 -0500

    dir

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Page from "./components/Page";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Page from "./components/Page/Page";
 
 function App() {
   return (
