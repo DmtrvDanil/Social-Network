a3ede3a2b826c9ea32a6eae78b938a5b615693f9
Danil <dmtrvd90@gmail.com>
2023-02-08 18:22:05 -0500

    users class init

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,47 +2,31 @@ import React from "react";
 import User from "./User/User";
 import axios from 'axios';
 
-const Users = (props) => {
 
-
-    if (props.usersPage.usersData.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            console.log(response.data.items);
-            props.setUsers(response.data.items);
-            // console.log(props.usersPage.usersData);
-        })
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         followStatus: false,
-        //         name: 'Danil',
-        //         age: '21',
-        //         location: {country: 'USA', city: 'California'},
-        //         photo: 'https://www.shutterstock.com/image-vector/cat-avatar-profile-picture-7-260nw-1660656721.jpg'
-        //     },
-        //     {
-        //         id: 2,
-        //         followStatus: true,
-        //         name: 'Tema',
-        //         age: '20',
-        //         location: {country: 'USA', city: 'California'},
-        //         photo: 'https://www.shutterstock.com/image-vector/cat-avatar-profile-picture-7-260nw-1660656721.jpg'
-        //     }
-        // ])
+class Users extends React.Component {
+    componentDidMount() {
+        if (this.props.usersPage.usersData.length === 0) {
+            alert("fe");
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items);
+            })
+        }
     }
-
-    let userElements = props.usersPage.usersData.map((user) => {
+    userElements =  this.props.usersPage.usersData.map((user) => {
         return (
-
-            <User userId={user.id} followStatus={user.followStatus} name={user.name} age={user.age} photo={user.photo} follow={props.onFollow} unfollow={props.onUnfollow}></User>
+            <User userId={user.id} followStatus={user.followStatus} name={user.name} age={user.age} photo={user.photo} follow={this.props.onFollow} unfollow={this.props.onUnfollow}></User>
         )
     })
 
-    return (
-        <div>
-            {userElements}
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                {this.userElements}
+            </div>
+        )
+    }
 }
+
+
 
 export default Users;
---
+++ src/components/Users/UsersFunc.jsx
@@ -0,0 +1,34 @@
+// import React from "react";
+// import User from "./User/User";
+// import axios from 'axios';
+//
+// const Users = (props) => {
+//
+//  let getUsers = () => {
+//      if (props.usersPage.usersData.length === 0) {
+//          axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+//              console.log(response.data.items);
+//              props.setUsers(response.data.items);
+//              // console.log(props.usersPage.usersData);
+//          })
+//      }
+//  }
+//
+//     let userElements = props.usersPage.usersData.map((user) => {
+//         return (
+//
+//             <User userId={user.id} followStatus={user.followStatus} name={user.name} age={user.age} photo={user.photo} follow={props.onFollow} unfollow={props.onUnfollow}></User>
+//         )
+//     })
+//
+//     return (
+//         <div>
+//             <button onClick={getUsers}>
+//                 Render users
+//             </button>
+//             {userElements}
+//         </div>
+//     )
+// }
+//
+// export default Users;
